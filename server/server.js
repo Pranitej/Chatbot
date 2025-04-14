319417a7132ba3c800d199dc599c2f5d75fc9382
Vangala Pranitej <137621721+Pranitej@users.noreply.github.com>
2025-04-14 12:36:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: {
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow all common methods
+  },
 });
 
 app.use(cors());
